d460c99d5343e0067bfd629eb1c7caa6ad96909f
Nikita Pandit <omotto896@gmail.com>
2025-05-18 12:51:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,69 +1,4 @@
 
-// const express = require("express");
-// const app = express();
-// const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
-// app.use(bodyParser.json());
-// const notificationRoutes = require("./routes/notificationRoutes");
-// const amqp = require("amqplib");
-// require("dotenv").config();
-
-
-
-
-// // RabbitMQ setup
-// let channel, connection;
-// async function connectRabbitMQ() {
-//   try {
-//     connection = await amqp.connect(process.env.RABBITMQ_URL);
-//     channel = await connection.createChannel();
-//     await channel.assertQueue("notificationQueue");
-//     console.log("Connected to RabbitMQ");
-    
-//     // Consumer
-//     channel.consume("notificationQueue", async (msg) => {
-//       if (msg !== null) {
-//         const data = JSON.parse(msg.content.toString());
-//         const { processNotification } = require("./services/notificationServices");
-//         try {
-//           await processNotification(data);
-//           channel.ack(msg);
-//         } catch (err) {
-//           console.error("Retrying message", err);
-//           if (data.retries < 3) {
-//             data.retries++;
-//             channel.sendToQueue("notificationQueue", Buffer.from(JSON.stringify(data)));
-//             channel.ack(msg);
-//           } else {
-//             console.error("Max retries reached, discarding message");
-//             channel.ack(msg);
-//           }
-//         }
-//       }
-//     });
-//   } catch (error) {
-//     console.error("Failed to connect to RabbitMQ", error);
-//   }
-// }
-// connectRabbitMQ();
-
-// app.use("/api", notificationRoutes);
-
-// // MongoDB connection
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((err) => console.log(err));
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// module.exports = { channel };
-
-
-
-
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -106,6 +41,10 @@ connectRabbitMQ().then(channel => {
 });
 
 app.use("/api", notificationRoutes);
+app.get('/', (req, res) => {
+  res.send('Notification Service Backend is running!');
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
